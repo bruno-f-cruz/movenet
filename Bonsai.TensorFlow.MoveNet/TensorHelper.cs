bb73f62f637f6f606c1b53c9ae192c5f99d1cf32
bruno-f-cruz <bfpcruz@gmail.com>
2023-02-17 23:06:24 +0000

    first commit

---
+++ Bonsai.TensorFlow.MoveNet/TensorHelper.cs
@@ -0,0 +1,99 @@
+﻿using OpenCV.Net;
+using System;
+using System.IO;
+using TensorFlow;
+
+namespace Bonsai.TensorFlow.MoveNet
+{
+    static class TensorHelper
+    {
+        public static TFGraph ImportModel(string fileName, out TFSession session)
+        {
+            using (var options = new TFSessionOptions())
+            {
+                unsafe
+                {
+
+                    byte[] GPUConfig = new byte[] { 0x32, 0x02, 0x20, 0x01 };
+                    fixed (void* ptr = &GPUConfig[0])
+                    {
+                        options.SetConfig(new IntPtr(ptr), GPUConfig.Length);
+                    }
+                }
+
+                var graph = new TFGraph();
+                var bytes = File.ReadAllBytes(fileName);
+                session = new TFSession(graph, options, null);
+                graph.Import(bytes);
+                return graph;
+            }
+        }
+
+        public static TFTensor CreatePlaceholder(TFGraph graph, TFSession.Runner runner, Size frameSize, int batchSize = 1, int TensorChannels = 1, TFDataType DType = TFDataType.UInt8)
+        {
+            int numberOfBytes;
+            switch (DType)
+            {
+                case TFDataType.UInt8:
+                    numberOfBytes = 1;
+                    break;
+                case TFDataType.Int32:
+                    numberOfBytes = 4;
+                    break;
+                default:
+                    throw new NotImplementedException("TFDataType not currently implemented");
+            }
+
+            var tensor = new TFTensor(
+                DType,
+                new long[] { batchSize, frameSize.Height, frameSize.Width, TensorChannels },
+                batchSize * frameSize.Width * frameSize.Height * TensorChannels * numberOfBytes);
+            runner.AddInput(graph["x"][0], tensor); 
+            return tensor;
+        }
+
+        public static IplImage EnsureFrameSize(IplImage frame, Size tensorSize, ref IplImage resizeTemp)
+        {
+            if (tensorSize != frame.Size)
+            {
+                if (resizeTemp == null || resizeTemp.Size != tensorSize)
+                {
+                    resizeTemp = new IplImage(tensorSize, frame.Depth, frame.Channels);
+                }
+
+                CV.Resize(frame, resizeTemp);
+                frame = resizeTemp;
+            }
+
+            return frame;
+        }
+
+        public static void UpdateTensor(TFTensor tensor, int TensorChannels, Depth DType, params IplImage[] frames)
+        {
+            var batchSize = (int)tensor.Shape[0];
+            var tensorRows = (int)tensor.Shape[1];
+            var tensorCols = (int)tensor.Shape[2];
+            if (frames?.Length != batchSize)
+            {
+                throw new ArgumentException("The number of frames does not match the tensor batch size.", nameof(frames));
+            }
+
+            using (var data = new Mat(batchSize * tensorRows, tensorCols, DType, TensorChannels, tensor.Data))
+            {
+                if (frames.Length == 1)
+                {
+                    CV.Convert(frames[0], data);
+                }
+                else
+                {
+                    for (int i = 0; i < frames.Length; i++)
+                    {
+                        var startRow = i * tensorRows;
+                        var image = data.GetRows(startRow, startRow + tensorRows);
+                        CV.Convert(frames[i], image);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,41 @@
+# Bonsai - MoveNet
+
+## How to install
+
+Bonsai.MoveNet can be downloaded through the Bonsai package manager. However, in order to use it for either CPU or GPU inference, you need to pair it with a compiled native TensorFlow binary. You can find precompiled binaries for Windows 64-bit at https://www.tensorflow.org/install/lang_c.
+
+To use GPU TensorFlow (highly recommended for live inference), you also need to install the `CUDA Toolkit` and the `cuDNN libraries`. The current package was developed and tested with [CUDA v11.3](https://developer.nvidia.com/cuda-11.3.0-download-archive) and [cuDNN 8.2](https://developer.nvidia.com/cudnn). Additionally, make sure you have a CUDA [compatible GPU](https://docs.nvidia.com/deploy/cuda-compatibility/index.html#support-hardware) with the latest NVIDIA drivers.
+
+After downloading the native TensorFlow binary and cuDNN, you can follow these steps to get the required native files into the `Extensions` folder of your local Bonsai install:
+
+1. The easiest way to find your Bonsai install folder is to right-click on the Bonsai shortcut > Properties. The path to the folder will be shown in the "Start in" textbox;
+2. Copy `tensorflow.dll` file from either the CPU or GPU [tensorflow release](https://www.tensorflow.org/install/lang_c#download_and_extract) to the `Extensions` folder;
+3. If you are using TensorFlow GPU, make sure to add the `cuda/bin` folder of your cuDNN download to the `PATH` environment variable, or copy all DLL files to the `Extensions` folder.
+
+## How to use
+
+The library already includes a pre-trained [MoveNet](https://tfhub.dev/google/movenet/singlepose/lightning/4) .pb file that is loaded when using the `PredictMoveNet` operator. In order to run inference on an incoming image, simply connect a node that provides an `IplImage` type to `PredictMoveNet`. E.g:
+
+![WorkflowExample](./Assets/workflow_example.svg)
+
+The output of the model (`Pose`) can be indexed using one of the following 17 human `bodypart` keypoints:
+
+```
+- nose
+- left_eye
+- right_eye
+- left_ear
+- right_ear
+- left_shoulder
+- right_shoulder
+- left_elbow
+- right_elbow
+- left_wrist
+- right_wrist
+- left_hip
+- right_hip
+- left_knee
+- right_knee
+- left_ankle
+- right_ankle
+```
